f4a62c5fe7138346ffa73d4ca80fbbc51d4d7d9c
himanshu Padhy <himanshupadhy4@gmail.com>
2025-09-02 09:52:53 +0530

    Create ADD 2 NUMBERS.c

---
+++ ADD 2 NUMBERS.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+   printf("ENTER THE A =");
+   scanf("%d",&a);
+      printf("ENTER THE B =");
+   scanf("%d",&b);
+   
+    int sum = a + b; 
+    printf("Sum of A and B = %d",sum);
+
+    return 0;
+}
