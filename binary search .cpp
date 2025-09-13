6cce690676a4c430ca5d25ac361add8b9d170430
himanshu Padhy <himanshupadhy4@gmail.com>
2025-09-13 09:05:10 +0530

    Create binary search .cpp

---
+++ binary search .cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={10,20,30,40,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int val = 50;
+    int index = -1;
+    int low = 0;
+    int high = n-1;
+    while(low <= high){
+    int mid = (low+ high)/2;
+    if(arr[mid]== val ){
+    index = mid;
+    break;
+    }
+    else if(arr[mid]<val){
+        low= mid+1;
+    }
+    else{
+    high = mid-1;
+    }}
+    if(index == -1){
+        cout<<"Not Fount"<<endl;
+    }
+    else{
+        cout<<"Fount the Index = "<< index;
+    }     }
